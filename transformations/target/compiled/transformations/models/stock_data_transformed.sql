497ad5a84df7f12d7fbade93cd4339e60020f68a
nikiljk <nikiljk@gmail.com>
2024-09-21 21:18:21 +0200

    current version of dbt works when stock_data_transformed does not exist. dbt runs successfully but there are still some circular dependancy errors

---
+++ transformations/target/compiled/transformations/models/stock_data_transformed.sql
@@ -0,0 +1,18 @@
+-- models/stock_data_transformed.sql
+
+with stock_data as (
+    select
+        ticker,
+        date_time,
+        close_price,
+        volume
+    from stock_data
+)
+
+select
+    ticker,
+    date_time,
+    close_price,
+    volume
+from stock_data
+where date_time >= date('now', '-7 days')  -- Example: last 7 days
---
+++ transformations/target/run/transformations/models/stock_data_transformed.sql
@@ -0,0 +1,21 @@
+
+    
+    create view main."stock_data_transformed" as
+    -- models/stock_data_transformed.sql
+
+with stock_data as (
+    select
+        ticker,
+        date_time,
+        close_price,
+        volume
+    from stock_data
+)
+
+select
+    ticker,
+    date_time,
+    close_price,
+    volume
+from stock_data
+where date_time >= date('now', '-7 days')  -- Example: last 7 days;
